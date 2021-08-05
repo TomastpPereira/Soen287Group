c9b7da041e26564c697e9c5dee73cd7333258e59
TomastpPereira <82472679+TomastpPereira@users.noreply.github.com>
2021-08-05 15:05:53 -0400

    First Work on Buttons for Cereal Aisle

---
+++ P3_Cereals_Script.js
@@ -0,0 +1,25 @@
+function hideDescription() {
+    var x = document.getElementById("More-Description");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function increaseCount(a, b) {
+    var input = b.previousElementSibling;
+    var value = parseInt(input.value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    input.value = value;
+}
+function decreaseCount(a, b){
+    var input = b.nextElementSibling;
+    var value = parseInt(input.value, 10);
+    if (value > 1) {
+        value = isNaN(value) ? 0 : value;
+        value--;
+        input.value = value;
+    }
+}
