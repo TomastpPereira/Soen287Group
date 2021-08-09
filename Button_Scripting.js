3a90fc93ea483ef9ddb63a78a1100c4c9df7bcd4
TomastpPereira <82472679+TomastpPereira@users.noreply.github.com>
2021-08-09 16:17:12 -0400

    Fixed links and adapted Cereal Aisle

---
+++ Button_Scripting.js
@@ -0,0 +1,44 @@
+  var price = 0.99; // set price of item
+    var max = 12; // set max of item
+
+    var count = 0;
+    var remove = document.getElementById("remove");
+    var add = document.getElementById("add");
+    var counterDiv = document.getElementById("qty");
+    var subtotal = document.getElementById("subtotal");
+
+    add.addEventListener('click', function () {
+        if(count < max) {
+            count += 1;
+            counterDiv.innerHTML = count;
+            updateSubtotal();
+        }
+    });
+
+    remove.addEventListener('click', function () {
+        if (count > 0) {
+            count -= 1;
+            counterDiv.innerHTML = count;
+            updateSubtotal();
+        }
+    });
+
+    function updateSubtotal() {
+        subtotal.innerHTML = "$" + (Math.round(count * price * 100) / 100).toFixed(2);
+    }
+
+
+    function unHideDescription() {
+        var showBtn = document.getElementById("moreDescriptionBtn");
+        var description = document.getElementById("description");
+
+        if (description.style.visibility == "hidden") {
+            description.style.visibility = "visible";
+            showBtn.innerText = "Collapse";
+
+        } else {
+            description.style.visibility = "hidden";
+            showBtn.innerText = "Show Detailed Description";
+        }
+
+    }
