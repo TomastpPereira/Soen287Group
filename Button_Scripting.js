f82089ffb20aab3073cfee067bfe510265aa6591
Nolan Bastien <n_astien@live.concordia.ca>
2021-08-10 13:45:47 -0400

    I updated the buttonJS to keep the Qty saved. It creates a different counter for each items. Everyone has to add id="item" to the name of their item for it to work. I also updated apple, banana and grapes accordingly.

--- Button_Scripting.js
+++ Button_Scripting.js
@@ -1,49 +1,69 @@
-  var price = 0.99; // set price of item
-    var max = 12; // set max of item
+var price = 0.99; // set price of item
+var max = 12; // set max of item
 
-    var count = 0;
-    var remove = document.getElementById("remove");
-    var add = document.getElementById("add");
-    var counterDiv = document.getElementById("qty");
-    var subtotal = document.getElementById("subtotal");
-    var addToCartBtn = document.getElementById("addToCart");
+var count;
+var remove = document.getElementById("remove");
+var add = document.getElementById("add");
+var counterDiv = document.getElementById("qty");
+var subtotal = document.getElementById("subtotal");
+var addToCartBtn = document.getElementById("addToCart");
+var element = document.getElementById("item");
+var itemName = element.innerText;
 
-    addToCartBtn.addEventListener('click', function() {
-        alert("Item added to cart!");
-    });
+// This is to keep count after refresh
 
-    add.addEventListener('click', function () {
-        if(count < max) {
-            count += 1;
-            counterDiv.innerHTML = count;
-            updateSubtotal();
-        }
-    });
+if(localStorage.getItem(itemName) == null) {
+    count = 0;
+    updateHtml();
+}
+else {
+    count = parseInt(localStorage.getItem(itemName));
+    updateHtml();
+}
 
-    remove.addEventListener('click', function () {
-        if (count > 0) {
-            count -= 1;
-            counterDiv.innerHTML = count;
-            updateSubtotal();
-        }
-    });
+// Alert on add to cart button
 
-    function updateSubtotal() {
-        subtotal.innerHTML = "$" + (Math.round(count * price * 100) / 100).toFixed(2);
+addToCartBtn.addEventListener('click', function () {
+    alert("Item added to cart!");
+});
+
+// Add and remove function
+
+add.addEventListener('click', function () {
+    if (count < max) {
+        count += 1;
+        updateHtml();
+    }
+});
+
+remove.addEventListener('click', function () {
+    if (count > 0) {
+        count -= 1;
+        updateHtml();
+    }
+});
+
+// Update html to update the text and the count in local storage
+
+function updateHtml() {
+    counterDiv.innerHTML = count;
+    subtotal.innerHTML = "$" + (Math.round(count * price * 100) / 100).toFixed(2);
+    localStorage.setItem(itemName, count);
+}
+
+// Hide and unhide description
+
+function unHideDescription() {
+    var showBtn = document.getElementById("moreDescriptionBtn");
+    var description = document.getElementById("description");
+
+    if (description.style.visibility == "hidden") {
+        description.style.visibility = "visible";
+        showBtn.innerText = "Collapse";
+
+    } else {
+        description.style.visibility = "hidden";
+        showBtn.innerText = "Show Detailed Description";
     }
 
-
-    function unHideDescription() {
-        var showBtn = document.getElementById("moreDescriptionBtn");
-        var description = document.getElementById("description");
-
-        if (description.style.visibility == "hidden") {
-            description.style.visibility = "visible";
-            showBtn.innerText = "Collapse";
-
-        } else {
-            description.style.visibility = "hidden";
-            showBtn.innerText = "Show Detailed Description";
-        }
-
-    }
+}
