c07e8050c924a3db3e7f04461ebb12a199cdf8b6
CSArie <87393395+CSArie@users.noreply.github.com>
2021-08-25 17:29:15 -0400

    Create admin_Navbar.php

---
+++ admin_Navbar.php
@@ -0,0 +1,20 @@
+<?php include('admin_Check.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel = "stylesheet type" type = "text/css" href= "Style_P1_P2.css" >
+    </head>
+
+<body>
+<div class="navbar">
+            <a class="active" href="index.html">Home</a>
+            <a href="P7_list_of_products.php"> Product List </a>
+            <!-- <a href="P8_Edit_A_Product.html"> Edit Product </a> -->
+            <a href="P9_Edit_User_List.html"> User List </a>
+            <!-- <a href="P10_Edit_User.html"> Edit User </a> -->
+            <a href="P11_Edit_Order_List.php"> Order List </a>
+            <!-- <a href="P12_Edit_an_Order_Profile.php"> Edit Order </a> -->
+        </div>
+</body>
+</html>
